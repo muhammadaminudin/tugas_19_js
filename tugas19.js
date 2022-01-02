0563626b6829469c50f763e6e6a40abe3f8e9e62
Muhammad Aminudin <aminudin199809@gmail.com>
2022-01-02 13:58:21 +0700

    tugas 19
    Mohon untuk di review

---
+++ tugas19.js
@@ -0,0 +1,16 @@
+function tugas() {
+  var kata = "Semangat...";
+  console.log(kata.repeat(5));
+
+  var bin1 = "**********";
+  console.log(bin1);
+  console.log(bin1.repeat(2));
+  console.log(bin1.repeat(3));
+  console.log(bin1.repeat(4));
+  console.log(bin1.repeat(5));
+  console.log(bin1.repeat(4));
+  console.log(bin1.repeat(3));
+  console.log(bin1.repeat(2));
+  console.log(bin1);
+}
+tugas();
